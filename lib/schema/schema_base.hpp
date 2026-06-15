9f2062f3b4be1b58e197bf662ff65f5d28d2c212
Isadora Ghannam <isa.gha@gmail.com>
2026-06-15 18:15:43 -0300

    add: minimal test

--- lib/schema/schema_base.hpp
+++ lib/schema/schema_base.hpp
@@ -6,7 +6,7 @@
 /*   By: ighannam <ighannam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/28 19:11:32 by brunofer          #+#    #+#             */
-/*   Updated: 2026/06/14 18:41:41 by ighannam         ###   ########.fr       */
+/*   Updated: 2026/06/15 18:12:22 by ighannam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -126,7 +126,7 @@ namespace schema
 					{
 						std::string msg = (*validators[i])(value);
 						if (!msg.empty())
-							errors.push_back(error(field_name, msg));	
+							errors.push_back(error(field_name, msg));
 					}
 					if (errors.empty())
 						return result<T>::success(value);
@@ -134,7 +134,10 @@ namespace schema
 				}
 				
 			public:
-				Derived& name(std::string _name) { field_name = _name; return static_cast<Derived&>(*this); };
+				Derived& name(std::string _name) {
+					field_name = _name;
+					return static_cast<Derived&>(*this);
+				};
 				Derived& refine(functor_refine* refine_validator, std::string msg)
 				{
 					struct functor_validator_refine: public functor_validator
@@ -163,6 +166,7 @@ namespace schema
 					{
 						for (size_t i = 0; i < other.validators.size(); i++)
 							this->validators.push_back(other.validators[i]->clone());
+						this->field_name = other.field_name;
 					}
 				}
 				schema_base& operator=(const schema_base& other)
@@ -174,6 +178,7 @@ namespace schema
 						validators.clear();
 						for (size_t i = 0; i < other.validators.size(); i++)
 							validators.push_back(other.validators[i]->clone());
+						this->field_name = other.field_name;
 					}
 					return *this;
 				}
